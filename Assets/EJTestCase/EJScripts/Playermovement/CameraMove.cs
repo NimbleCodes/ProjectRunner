6089ab88acc420a040ac93a9752f56cf96707544
EUNJI0114 <132033152+EUNJI0114@users.noreply.github.com>
2023-06-16 12:01:04 +0900

    camera
    3인칭 카메라 하다가 말음

---
+++ Assets/EJTestCase/EJScripts/Playermovement/CameraMove.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    [Header("Refernces")]
+    public Transform orientation;
+    public Transform player;
+    public Transform playerObj;
+    public Rigidbody rb;
+
+    public float rotationSpeed;
+
+    public Transform playerlookAt;
+
+    public CameraStyle currentstyle; 
+    public enum CameraStyle
+    {
+        Basic,
+        Combat
+    }
+    
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 viewDir = player.position - new Vector3(transform.position.x, player.position.y, transform.position.z);
+        orientation.forward = viewDir.normalized;
+
+        if (currentstyle == CameraStyle.Basic)
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            Vector3 inputDir = orientation.forward * horizontalInput;
+
+            if (inputDir != Vector3.zero)
+                playerObj.forward = Vector3.Slerp(playerObj.forward, inputDir.normalized, Time.deltaTime * rotationSpeed);
+
+        }
+        else if(currentstyle == CameraStyle.Combat)
+        {
+            Vector3 dirToCombatLookAt = playerlookAt.position - new Vector3(transform.position.x, playerlookAt.position.y, transform.position.z); 
+            orientation.forward = dirToCombatLookAt.normalized;
+
+            playerObj.forward = dirToCombatLookAt.normalized;
+        }
+       
+    }
+
+
+
+    
+}
