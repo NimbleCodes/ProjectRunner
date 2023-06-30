0baaf46f7d59264889a1a7b42746c282e04e0144
EUNJI0114 <132033152+EUNJI0114@users.noreply.github.com>
2023-06-30 11:55:08 +0900

    23.06.30
    아이템 집기

--- Assets/EJTestCase/EJScripts/Playermovement/GetItem.cs
+++ Assets/EJTestCase/EJScripts/Playermovement/GetItem.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,11 +10,19 @@ public class GetItem : MonoBehaviour
     [SerializeField] Transform _cam;
     [SerializeField] GameObject _getItemPanel;
     [SerializeField] GameObject[] _itemSlot;
+    [SerializeField] Transform _Weapons; 
     public float _playerActionDistance;
     public bool active = false;
     RaycastHit hit;
+    GameObject _holding, _objName;
 
     void Update()
+    {
+        EatItem();
+        UseItem(); 
+    }
+
+    private void EatItem()
     {
         active = Physics.Raycast(_cam.position, _cam.TransformDirection(Vector3.forward), out hit, _playerActionDistance);
         if (active == true && hit.collider.CompareTag("Weapon"))
@@ -26,10 +36,31 @@ public class GetItem : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.F) && active == true)
         {
             _itemSlot[0].gameObject.SetActive(true);
-            _itemSlot[0].GetComponent<Image>().sprite = Resources.Load<Sprite>("Texture/ItemIcons/"+ hit.collider.name); ;
-            Destroy(hit.collider.gameObject); 
+            _itemSlot[0].GetComponent<Image>().sprite = Resources.Load<Sprite>("Texture/ItemIcons/" + hit.collider.name);
+            _objName = hit.collider.gameObject; 
+            Destroy(hit.collider.gameObject);
         }
-
     }
 
+    private void UseItem()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            GameObject obj = Resources.Load<GameObject>("Prefabs/" + _itemSlot[0].GetComponent<Image>().sprite.name);
+            if(obj != null)
+            {
+                _holding = Instantiate(obj, _Weapons);
+                _holding.transform.localPosition = _Weapons.transform.localPosition;
+               
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+
+        }
+    }
 }
