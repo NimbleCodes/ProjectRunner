e869e647f4a3a37cd4c53cab70790e847c21a0f7
EUNJI0114 <132033152+EUNJI0114@users.noreply.github.com>
2023-07-07 11:11:39 +0900

    0707-2

--- Assets/EJTestCase/EJScripts/ObjectsScripts/PickUpItem.cs
+++ Assets/EJTestCase/EJScripts/ObjectsScripts/PickUpItem.cs
@@ -1,18 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PickUpItem : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    RaycastHit hit;
+    public Rigidbody rig;
+    public Collider coll; 
+    public Transform Player, FpsCam;
 
-    // Update is called once per frame
+    public float pickUpRange;
+    public bool equipped;
+    public static bool slotfull;
+
+    [SerializeField] GameObject[] _itemSlot;
+    [SerializeField] Transform _Weapons;
+    GameObject _holding; 
+
     void Update()
     {
-        
+        //Vector3 distanceToPlayer = hit.collider.transform.position - transform.position;
+        //if (!equipped && distanceToPlayer.magnitude <= pickUpRange && !slotfull)
+        //{
+            //PickItem();
+        //}
+
+        PickItem(); 
+    }
+
+    private void PickItem()
+    {
+        equipped = true;
+        slotfull = true;
+        rig.isKinematic = true;
+        coll.isTrigger = true;
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            GameObject obj = Resources.Load<GameObject>("Prefabs/" + _itemSlot[0].GetComponent<Image>().sprite.name);
+            obj.transform.SetParent(_Weapons.transform); 
+            //_holding = Instantiate(obj, _Weapons);
+            obj.transform.localPosition = _Weapons.transform.localPosition;
+            obj.transform.localRotation = _Weapons.transform.localRotation;
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+
+        }
     }
 }
