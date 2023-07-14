07fa0bc57f5193ba0edcfa7d75925fc6a171c99f
EUNJI0114 <132033152+EUNJI0114@users.noreply.github.com>
2023-07-14 12:02:59 +0900

    0714

--- Assets/TestCase/Scripts/Movement/Movement.cs
+++ Assets/TestCase/Scripts/Movement/Movement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Movement : MonoBehaviour
 {
@@ -13,19 +14,68 @@ public class Movement : MonoBehaviour
     }
 
 
-    void FixedUpdate()
+    void Update()
     {
-        MoveLocalTransform(); 
+        MoveLocalTransform();
     }
 
     void MoveLocalTransform()
     {
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
+
         Vector3 vPos = transform.position;
         vPos += transform.right * x * Time.deltaTime * _speed;
         vPos += transform.forward * y * Time.deltaTime * _speed;
         transform.position = vPos;
-    }
+  
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            this.transform.rotation = Quaternion.Euler(0, 0, 0);
+            GetComponent<Animator>().SetBool("IsRun", true);
+            GetComponent<Animator>().Play("FastRun");
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            this.transform.rotation = Quaternion.Euler(0, 0, 0);
+            GetComponent<Animator>().SetBool("IsBack", true);
+            GetComponent<Animator>().Play("RunningBack");
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            GetComponent<Animator>().SetBool("IsRun", false);
+            GetComponent<Animator>().Play("Idle");
+        }
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            GetComponent<Animator>().SetBool("IsRun", false);
+            GetComponent<Animator>().Play("Idle");
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.rotation = Quaternion.Euler(0, 90, 0);
+            GetComponent<Animator>().SetBool("IsRun", true); 
+            GetComponent<Animator>().Play("FastRun");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.rotation = Quaternion.Euler(0, -90, 0);
+            GetComponent<Animator>().SetBool("IsRun", true);
+            GetComponent<Animator>().Play("FastRun");
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            GetComponent<Animator>().SetBool("IsRun", false);
+            GetComponent<Animator>().Play("Idle");
+        }
 
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            GetComponent<Animator>().SetBool("IsRun", false);
+            GetComponent<Animator>().Play("Idle");
+        }
+    }
+        
 }
+
+
