b56cdc4092437e23f6e0b192b6c24d8f609234b1
EUNJI0114 <132033152+EUNJI0114@users.noreply.github.com>
2023-09-01 11:54:39 +0900

    0901

--- Assets/EJTestCase/EJScripts/BossScripts/BossAttack.cs
+++ Assets/EJTestCase/EJScripts/BossScripts/BossAttack.cs
@@ -1,3 +1,5 @@
+using Cinemachine.Utility;
+using System.Collections;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -8,30 +10,30 @@ public class BossAttack : MonoBehaviour
     [SerializeField] Transform _Boss;
     [SerializeField] Transform[] weaponPoint;
     [SerializeField] GameObject[] _weapon;
-    [SerializeField] Animator _ani; 
-    public float throwPower; 
+    [SerializeField] Animator _ani;
+    GameObject _temp;
+    [SerializeField] float throwPower;
+    private float time;
     private bool equipped = false;
 
     void Update()
     {
-        LookPlayer();
+        LookAt();
+    }
+    private void Start()
+    {
         Spawn();
+        StartCoroutine(ThrowObject());
     }
 
-    void LookPlayer()
+    void LookAt()
     {
-        transform.LookAt(_player);
+        _Boss.LookAt(_player);
     }
+
 
     void Spawn()
     {
-        Vector3 target; 
-        float distance = Vector3.Distance(_player.position, _Boss.position);
-
-        Ray ray = new Ray(); 
-        RaycastHit playerhit;
-        int PlayerMask = LayerMask.GetMask("Player"); 
-        
 
         if (equipped == false)
         {
@@ -41,33 +43,44 @@ public class BossAttack : MonoBehaviour
             int selectedPoint = Random.Range(0, weaponPoint.Length);
             Transform selectedSpot = weaponPoint[selectedPoint];
 
-            GameObject instance = Instantiate(selectedWeapon);
-            instance.transform.SetParent(selectedSpot);
-            instance.transform.localPosition = Vector3.zero;
-            instance.transform.localRotation = Quaternion.Euler(Vector3.zero);
-            instance.transform.localRotation = Quaternion.Euler(new Vector3(0, 180, 0));
+            _temp = Instantiate(selectedWeapon);
+            _temp.transform.SetParent(selectedSpot);
+            _temp.transform.localPosition = Vector3.zero;
+            _temp.transform.localRotation = Quaternion.Euler(new Vector3(0, 180, 0));
 
-            instance.GetComponent<Rigidbody>().isKinematic = true;
-            instance.GetComponent<BoxCollider>().isTrigger = true;
+            _temp.GetComponent<Rigidbody>().isKinematic = true;
+            _temp.GetComponent<BoxCollider>().isTrigger = true;
 
             equipped = true;
+        }
 
-            if (Physics.Raycast(ray, out playerhit, 20f, PlayerMask))
+    }
+
+    IEnumerator ThrowObject() //2.5 초마다 생성된 오브젝트 던지기
+    {
+        
+        while (true)
+        {
+            Vector3 target = _player.position - _Boss.position;
+            float distance = Vector3.Distance(_player.position, _Boss.position);
+
+            if (distance < 20f && equipped)
             {
-                target =  (playerhit.point - selectedSpot.position).normalized;
-                instance.transform.SetParent(null);
-                target.y = 25f;
-                instance.GetComponent<Rigidbody>().isKinematic = false;
-                instance.GetComponent<Rigidbody>().AddForce(target * throwPower, ForceMode.Impulse);
-                
+                _temp.transform.SetParent(null);
+                _temp.GetComponent<Rigidbody>().isKinematic = false;
+                _temp.GetComponent<BoxCollider>().isTrigger = false;
+                _temp.GetComponent<Rigidbody>().AddForce(target * throwPower, ForceMode.Impulse);
+                _temp.GetComponent<Rigidbody>().AddForce(_temp.transform.up * 6f , ForceMode.Impulse); 
+
                 equipped = false;
             }
+            
+            
+            yield return new WaitForSeconds(2.5f);
+
+            Spawn();
+
         }
     }
 
-
-    void FinalAttack()
-    {
-
-    }
 }
