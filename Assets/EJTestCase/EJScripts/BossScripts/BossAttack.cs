30847471939c063643851561af339dafbc2fefae
EUNJI0114 <132033152+EUNJI0114@users.noreply.github.com>
2023-09-07 22:05:33 +0900

    0907

--- Assets/EJTestCase/EJScripts/BossScripts/BossAttack.cs
+++ Assets/EJTestCase/EJScripts/BossScripts/BossAttack.cs
@@ -1,20 +1,17 @@
 using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BossAttack : MonoBehaviour
 {
     [SerializeField] Transform _player;
     [SerializeField] Transform _Boss;
-    [SerializeField] Transform[] weaponPoint;
+    [SerializeField] Transform weaponPoint;
     [SerializeField] GameObject[] _weapon;
     [SerializeField] Animator _ani;
     GameObject _temp;
     [SerializeField] float throwPower;
     private bool equipped = false;
-
-    // 들고 있지 않으면 스폰 
-    // 애니메이션을 2.5초 마다 실행 
-    // 트리거에 부딪히면 던지기 실행 
 
     private void Start()
     {
@@ -42,8 +39,7 @@ public class BossAttack : MonoBehaviour
             int selection = Random.Range(0, _weapon.Length);
             GameObject selectedWeapon = _weapon[selection];
 
-            int selectedPoint = Random.Range(0, weaponPoint.Length);
-            Transform selectedSpot = weaponPoint[selectedPoint];
+            Transform selectedSpot = weaponPoint;
 
             _temp = Instantiate(selectedWeapon);
             _temp.transform.SetParent(selectedSpot);
@@ -60,27 +56,30 @@ public class BossAttack : MonoBehaviour
 
     IEnumerator ThrowObject() //2.5 초마다 생성된 오브젝트 던지기
     {
-
         while (true)
         {
             Vector3 target = _player.position - _Boss.position;
             float distance = Vector3.Distance(_player.position, _Boss.position);
-
+            
             if (distance < 20f && equipped)
             {
-                _ani.SetTrigger("IsThrow");
-                _temp.transform.SetParent(null);
-                _temp.GetComponent<Rigidbody>().isKinematic = false;
-                _temp.GetComponent<BoxCollider>().isTrigger = false;
-                _temp.GetComponent<Rigidbody>().AddForce(target * throwPower, ForceMode.Impulse);
-                _temp.GetComponent<Rigidbody>().AddForce(_temp.transform.up * 6f , ForceMode.Impulse); 
-
-                equipped = false;
+                Invoke("RealThrow", 3f);
             }
+            Invoke("Spawn", 1f);
             yield return new WaitForSeconds(2.5f);
-            Spawn();
-
         }
+    }
+
+    void RealThrow()
+    {
+        Vector3 target = _player.position - _Boss.position;
+        _temp.transform.SetParent(null);
+        _temp.GetComponent<Rigidbody>().isKinematic = false;
+        _temp.GetComponent<BoxCollider>().isTrigger = false;
+        _temp.GetComponent<Rigidbody>().AddForce(target * throwPower, ForceMode.Impulse);
+        _temp.GetComponent<Rigidbody>().AddForce(_temp.transform.up * 6f, ForceMode.Impulse);
+
+        equipped = false;
     }
 
 }
