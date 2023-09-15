fad3b83f541c48b045bb4cb2138b790ded88a421
EUNJI0114 <132033152+EUNJI0114@users.noreply.github.com>
2023-09-15 11:24:32 +0900

    0915

--- Assets/EJTestCase/EJScripts/Playermovement/PlayerAniEvent.cs
+++ Assets/EJTestCase/EJScripts/Playermovement/PlayerAniEvent.cs
@@ -1,11 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAniEvent : MonoBehaviour
 {
+    void AttackStart()
+    {
+        this.transform.rotation = Quaternion.Euler(0f, -70f, 0f);
+    }
+
     void AttackAniFin()
     {
         GetComponent<Animator>().SetLayerWeight(1, 1);
+        this.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
     }
 }
