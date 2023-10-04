b2a426d791203193f59d544d05b25f0a770ba127
EUNJI0114 <132033152+EUNJI0114@users.noreply.github.com>
2023-10-05 00:27:25 +0900

    1005 UI 이미지

--- Assets/EJTestCase/EJScripts/Playermovement/PlayerInBossMap.cs
+++ Assets/EJTestCase/EJScripts/Playermovement/PlayerInBossMap.cs
@@ -5,11 +5,15 @@ public class PlayerInBossMap : MonoBehaviour
     AudioSource _audio;
     [SerializeField] AudioClip _BossMap;
     GameObject _Inven;
+    public GameObject[] Enemyz = null;
+    GameObject _BossTrigger; 
 
     private void Start()
     {
         _audio = GameObject.FindGameObjectWithTag("BGM").GetComponent<AudioSource>();
         _Inven = GameObject.FindGameObjectWithTag("Inven");
+        Enemyz = GameObject.FindGameObjectsWithTag("Enemy");
+        _BossTrigger = GameObject.FindGameObjectWithTag("BossTrigger");
     }
 
     private void OnCollisionEnter(Collision other)
@@ -18,6 +22,13 @@ public class PlayerInBossMap : MonoBehaviour
         {
             _Inven.GetComponent<Inventory>().ResetItem();
             _audio.clip = _BossMap;
+
+            for (int i = 0; i < Enemyz.Length; i++)
+            {
+                Enemyz[i].SetActive(false);
+            }
+
+            _BossTrigger.SetActive(false);
         }
     }
 }
